3ceb2f2efaddcb64387f08a295839070a870ca36
Kalimaha <guido.barbaglia@gmail.com>
2015-08-25 16:44:17 +0200

    Project dependencies added.

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,52 +8,27 @@
 
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    //jshint: {
-    //  all: [
-    //    'Gruntfile.js',
-    //    'tasks/*.js',
-    //    '<%= nodeunit.tests %>'
-    //  ],
-    //  options: {
-    //    jshintrc: '.jshintrc'
-    //  }
-    //},
-    //
-    //// Before generating any new files, remove any previously-created files.
-    //clean: {
-    //  tests: ['tmp']
-    //},
+    /* Project configuration. */
+    grunt.initConfig({
 
-    // Configuration to be run (and then tested).
-    jsonschema_amd_restclient_generator: {
-      custom_options: {
-        options: {
-          base_url: 'http://localhost:8080/faostat-api/v1.0/',
-          output_name: 'faostat-api'
+        /* Plugin configuration. */
+        jsonschema_amd_restclient_generator: {
+            custom_options: {
+                options: {
+                    base_url: 'http://localhost:8080/faostat-api/v1.0/',
+                    output_name: 'test-client'
+                }
+            }
         }
-      }
-    }
 
-    // Unit tests.
-    //nodeunit: {
-    //  tests: ['test/*_test.js']
-    //}
+    });
 
-  });
+    /* Actually load this plugin's task(s). */
+    grunt.loadTasks('tasks');
 
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
-
-  // These plugins provide necessary tasks.
-  //grunt.loadNpmTasks('grunt-contrib-jshint');
-  //grunt.loadNpmTasks('grunt-contrib-clean');
-
-
-  /* Test task. */
-  grunt.registerTask('default', ['jsonschema_amd_restclient_generator']);
+    /* Test task. */
+    grunt.registerTask('default', ['jsonschema_amd_restclient_generator']);
 
 };
